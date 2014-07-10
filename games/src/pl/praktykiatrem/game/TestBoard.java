3db5ae56779006b23d88b1187894cda388ca9b81
filipr <filipr@KOM290.ATREM.PL>
2014-07-10 11:32:27 +0200

    commit test

--- games/src/pl/praktykiatrem/game/TestBoard.java
+++ games/src/pl/praktykiatrem/game/TestBoard.java
@@ -3,7 +3,9 @@ package pl.praktykiatrem.game;
 public class TestBoard {
 	
 	public static void main(String[] args) {
+	    Board plansza = new Board(5, 5);
 	    
+	    System.out.println(plansza);
     }
 
 }
