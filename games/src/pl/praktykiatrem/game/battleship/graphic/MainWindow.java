642bb546023ffc94fbe65f492efc45b976344356
filipr <filipr@KOM290.ATREM.PL>
2014-07-15 11:50:49 +0200

    nie bangla

---
+++ games/src/pl/praktykiatrem/game/battleship/graphic/MainWindow.java
@@ -0,0 +1,50 @@
+/*
+ * Plik stworzony dnia 15 lip 2014 przez filipr
+ *
+ * Copyright ATREM S.A.
+ * ATREM 2014(C)
+ */
+
+package pl.praktykiatrem.game.battleship.graphic;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainWindow {
+
+    private JFrame frame;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainWindow window = new MainWindow();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public MainWindow() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
