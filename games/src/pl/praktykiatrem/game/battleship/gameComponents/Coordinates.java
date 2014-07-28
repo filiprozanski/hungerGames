abf77f5a3ccc9ea3b1a44dd1b26e4bf6ff8cc0ed
filiprozanski <filip.rose@gmail.com>
2014-07-28 14:51:02 +0200

    dodane equals do Coordinates

--- games/src/pl/praktykiatrem/game/battleship/gameComponents/Coordinates.java
+++ games/src/pl/praktykiatrem/game/battleship/gameComponents/Coordinates.java
@@ -2,39 +2,46 @@ package pl.praktykiatrem.game.battleship.gameComponents;
 
 /**
  * Klasa przechowuje koordynaty
+ * 
  * @author Filip Ró¿añski
  *
  */
 public class Coordinates {
-	private int x;
-	private int y;
-	
-	/**
-	 * 
-	 * @param x wspó³rzêdna x
-	 * @param y wspó³rzêdna y
-	 */
-	public Coordinates(int x, int y)
-	{
-		this.x = x;
-		this.y = y;
-	}
-	
-	/**
-	 * 
-	 * @return wspó³rzêdna x
-	 */
-	public int getX()
-	{
-		return x;
-	}
-	
-	/**
-	 * 
-	 * @return wspo³rzêdna y
-	 */
-	public int getY()
-	{
-		return y;
-	}
+    private int x;
+    private int y;
+
+    /**
+     * 
+     * @param x
+     *            wspó³rzêdna x
+     * @param y
+     *            wspó³rzêdna y
+     */
+    public Coordinates(int x, int y) {
+	this.x = x;
+	this.y = y;
+    }
+
+    /**
+     * 
+     * @return wspó³rzêdna x
+     */
+    public int getX() {
+	return x;
+    }
+
+    /**
+     * 
+     * @return wspo³rzêdna y
+     */
+    public int getY() {
+	return y;
+    }
+
+    public boolean equals(Coordinates other) {
+	if (this.x == other.x && this.y == other.y)
+	    return true;
+	else
+	    return false;
+    }
 }
