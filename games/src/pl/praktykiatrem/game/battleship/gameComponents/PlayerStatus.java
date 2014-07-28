133987aff34815715fab96f46960c342eda55386
filiprozanski <filip.rose@gmail.com>
2014-07-28 13:41:33 +0200

    zaimplementowane Coordinates

--- games/src/pl/praktykiatrem/game/battleship/gameComponents/PlayerStatus.java
+++ games/src/pl/praktykiatrem/game/battleship/gameComponents/PlayerStatus.java
@@ -15,144 +15,151 @@ package pl.praktykiatrem.game.battleship.gameComponents;
  *
  */
 public class PlayerStatus {
-	private Board plansza;
-	private Ship[] ships;
-	private int shipTypes[];
-	private int shipsNumber;
-	private Player gamer;
+    private Board plansza;
+    private Ship[] ships;
+    private int shipTypes[];
+    private int shipsNumber;
+    private Player gamer;
 
-	// private String name;
+    // private String name;
 
-	/**
-	 * 
-	 * @param boardSize_x
-	 *            liczba miejsc planszy w poziome
-	 * @param boardSize_y
-	 *            liczba miejsc planszy w pionie
-	 * @param shipsNumber
-	 *            liczba statków gracza pozosta³a do zbicia na planszy
-	 * @param shipTypes
-	 *            tablica przechowuj¹ca informacje o typie statków u¿ywanych w
-	 *            rozgrywce
-	 */
-	public PlayerStatus(int boardSizeH, int boardSizeV,
-			int shipTypes[]) {
-		this.shipsNumber = shipTypes.length;
-		this.shipTypes = shipTypes;
-		plansza = new Board(boardSizeH, boardSizeV);
-		ships = new Ship[shipsNumber];
-		for (int i = 0; i < shipsNumber; i++)
-			setShip(i, shipTypes[i]);
-		gamer = new Player();
-	}
+    /**
+     * 
+     * @param boardSize_x
+     *            liczba miejsc planszy w poziome
+     * @param boardSize_y
+     *            liczba miejsc planszy w pionie
+     * @param shipsNumber
+     *            liczba statków gracza pozosta³a do zbicia na planszy
+     * @param shipTypes
+     *            tablica przechowuj¹ca informacje o typie statków u¿ywanych w
+     *            rozgrywce
+     */
+    public PlayerStatus(int boardSizeH, int boardSizeV, int shipTypes[]) {
+	this.shipsNumber = shipTypes.length;
+	this.shipTypes = shipTypes;
+	plansza = new Board(boardSizeH, boardSizeV);
+	ships = new Ship[shipsNumber];
+	for (int i = 0; i < shipsNumber; i++)
+	    setShip(i, shipTypes[i]);
+	gamer = new Player();
+    }
 
-	/**
-	 * 
-	 * @return imiê gracza
-	 */
-	public String getName() {
-		return gamer.getName();
-	}
+    /**
+     * 
+     * @return imiê gracza
+     */
+    public String getName() {
+	return gamer.getName();
+    }
 
-	/**
-	 * Ustawia imiê gracza
-	 * 
-	 * @param name
-	 */
-	public void setName(String name) {
-		gamer.setName(name);
-	}
+    /**
+     * Ustawia imiê gracza
+     * 
+     * @param name
+     */
+    public void setName(String name) {
+	gamer.setName(name);
+    }
 
-	/**
-	 * 
-	 * @return liczbe statków gracza pozosta³ych na planszy
-	 */
-	public int getShipsNumber() {
-		return shipsNumber;
-	}
+    /**
+     * 
+     * @return liczbe statków gracza pozosta³ych na planszy
+     */
+    public int getShipsNumber() {
+	return shipsNumber;
+    }
 
-	/**
-	 * 
-	 * @return zwraca plansze gracza
-	 */
-	public Board getPlansza() {
-		return plansza;
-	}
+    /**
+     * 
+     * @return zwraca plansze gracza
+     */
+    public Board getPlansza() {
+	return plansza;
+    }
 
-	/**
-	 * zmniejsza liczbê statków po zbiciu o 1
-	 */
-	public void reduceShipsNumber() {
-		shipsNumber--;
-	}
+    /**
+     * zmniejsza liczbê statków po zbiciu o 1
+     */
+    public void reduceShipsNumber() {
+	shipsNumber--;
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @param polesNumber
-	 *            liczba masztów statku
-	 */
-	public void setShip(int id, int polesNumber) {
-		ships[id] = new Ship(polesNumber);
-	}
+    /**
+     * 
+     * @param id
+     * @param polesNumber
+     *            liczba masztów statku
+     */
+    public void setShip(int id, int polesNumber) {
+	ships[id] = new Ship(polesNumber);
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @return statek o podanym id
-	 */
-	public Ship getShip(int id) {
-		return ships[id];
-	}
+    /**
+     * 
+     * @param id
+     * @return statek o podanym id
+     */
+    public Ship getShip(int id) {
+	return ships[id];
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @return statek o podanym id
-	 */
-	public boolean isShipSet(int id) {
-		return ships[id].isShipSet();
-	}
+    /**
+     * 
+     * @param id
+     * @return statek o podanym id
+     */
+    public boolean isShipSet(int id) {
+	return ships[id].isShipSet();
+    }
 
-	/**
-	 * deaktywuje na planszy miejsce opisane parametrami
-	 * 
-	 * @param x
-	 *            wspó³rzêdna pozioma
-	 * @param y
-	 *            wspó³rzêdna pionowa
-	 */
-	public void takeOutShip(int x, int y) {
-		plansza.takeOut(x, y);
-	}
+    /**
+     * deaktywuje na planszy miejsce opisane parametrami
+     * 
+     * @param x
+     *            wspó³rzêdna pozioma
+     * @param y
+     *            wspó³rzêdna pionowa
+     */
+    public void takeOutShip(int x, int y) {
+	plansza.takeOut(x, y);
+    }
 
-	/**
-	 * 
-	 * @param i
-	 * @return zwraca liczbe masztów statku znajduj¹cego siê w tablicy pod
-	 *         indeksem i
-	 */
-	public int getShipTypes(int i) {
-		return shipTypes[i];
-	}
+    /**
+     * 
+     * @param i
+     * @return zwraca liczbe masztów statku znajduj¹cego siê w tablicy pod
+     *         indeksem i
+     */
+    public int getShipTypes(int i) {
+	return shipTypes[i];
+    }
 
-	/**
-	 * zmniejsza liczbe masztów o jeden
-	 * 
-	 * @param x
-	 * @param y
-	 */
-	public boolean reducePolesNumber(int x, int y) {
-		return plansza.getPlace(x, y).getShip().reducePolesNumber();
-	}
+    /**
+     * zmniejsza liczbe masztów o jeden
+     * 
+     * @param x
+     * @param y
+     */
+    public boolean reducePolesNumber(int x, int y) {
+	return plansza.getPlace(x, y).getShip().reducePolesNumber();
+    }
 
-	/**
-	 * dodaje koordynaty do listy znajduj¹cej siê w statku
-	 * 
-	 * @param ID
-	 * @param c
-	 */
-	public void addCoordinates(int ID, Coordinates c) {
-		ships[ID].addCoordinate(c);
-	}
+    /**
+     * dodaje koordynaty do listy znajduj¹cej siê w statku
+     * 
+     * @param ID
+     * @param c
+     */
+    public void addCoordinates(int ID, Coordinates c) {
+	ships[ID].addCoordinate(c);
+    }
+
+    public void removeCoordinate(int ID, Coordinates c) {
+	ships[ID].removeCoordinate(c);
+    }
+
+    public Coordinates[] getCoordsTable(int ID) {
+	return ships[ID].getCoordsTable();
+    }
 }
--- games/src/pl/praktykiatrem/game/battleship/rules/IRules.java
+++ games/src/pl/praktykiatrem/game/battleship/rules/IRules.java
@@ -4,19 +4,21 @@ import pl.praktykiatrem.game.battleship.gameComponents.PlayerStatus;
 
 public interface IRules {
 
-	public boolean getCurrentPlayer();
+    public boolean getCurrentPlayer();
 
-	public boolean placeShips(PlayerStatus p, int id, int polesNumber,
-			Direction direction, int x, int y);
+    public boolean placeShips(PlayerStatus p, int id, int polesNumber,
+	    Direction direction, int x, int y);
 
-	public boolean makeMove(PlayerStatus p, int x, int y);
+    public boolean makeMove(PlayerStatus p, int x, int y);
 
-	public boolean displaceShips(PlayerStatus player, int id, int polesNumber,
-			Direction direction, int x, int y);
+    public boolean displaceShips(PlayerStatus player, int id, int polesNumber,
+	    Direction direction, int x, int y);
 
-	public int getBoardSize_H();
+    public int getBoardSize_H();
 
-	public int getBoardSize_V();
+    public int getBoardSize_V();
 
-	public int[] getShipTypes();
+    public int[] getShipTypes();
+
+    public int getShipsNumber();
 }
