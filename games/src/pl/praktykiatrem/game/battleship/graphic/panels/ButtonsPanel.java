ece7267db53a74a4462b32208fbbd4da5ad388c2
wertwiktor <wertwiktor@gmail.com>
2014-07-30 09:49:42 +0200

    buttony przy ustawianiu

--- games/src/pl/praktykiatrem/game/battleship/graphic/panels/ButtonsPanel.java
+++ games/src/pl/praktykiatrem/game/battleship/graphic/panels/ButtonsPanel.java
@@ -11,28 +11,34 @@ import pl.praktykiatrem.game.battleship.graphic.listeners.UserStageListener;
 import pl.praktykiatrem.game.battleship.graphic.observers.IStageObserver;
 
 public class ButtonsPanel extends JPanel {
-    private JButton ready;
+	private JButton ready;
+	private JButton reset;
+	private JButton autoSet;
 
-    public ButtonsPanel(IStageObserver observer) {
-	super(new GridLayout(1, 0));
-	initialize(observer);
-    }
+	public ButtonsPanel(IStageObserver observer) {
+		super(new GridLayout(3, 0));
+		initialize(observer);
+	}
 
-    private void initialize(IStageObserver observer) {
-	ready = new JButton("Gotowy!");
-	ready.addActionListener(new UserStageListener(observer));
-	add(ready, BorderLayout.CENTER);
-    }
+	private void initialize(IStageObserver observer) {
+		ready = new JButton("Gotowy!");
+		reset = new JButton("Resetuj planszê!");
+		autoSet = new JButton("Ustaw statki automatycznie!");
+		ready.addActionListener(new UserStageListener(observer));
+		add(ready, BorderLayout.CENTER);
+		add(autoSet, BorderLayout.CENTER);
+		add(reset, BorderLayout.CENTER);
+	}
 
-    public Dimension gerPrefferedSize() {
-	return new Dimension(150, 150);
-    }
+	public Dimension gerPrefferedSize() {
+		return new Dimension(150, 150);
+	}
 
-    public Dimension gerMinumumSize() {
-	return new Dimension(150, 150);
-    }
+	public Dimension gerMinumumSize() {
+		return new Dimension(150, 150);
+	}
 
-    public void diableButton() {
-	ready.setEnabled(false);
-    }
+	public void diableButton() {
+		ready.setEnabled(false);
+	}
 }
