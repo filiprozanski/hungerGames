f37c4e246aae5810424b3e14d4292827bb2d6321
filiprozanski <filip.rose@gmail.com>
2014-08-01 14:23:17 +0200

    ok

---
+++ games/src/pl/praktykiatrem/game/battleship/graphic/menu/testMenu.java
@@ -0,0 +1,15 @@
+package pl.praktykiatrem.game.battleship.graphic.menu;
+
+import javax.swing.JFrame;
+
+public class testMenu {
+
+	public static void main(String[] args) {
+		MainMenu f = new MainMenu();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(640, 450);
+		f.setVisible(true);
+
+	}
+
+}
