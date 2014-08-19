4e6cc15b121dd5a962c334b575e591a1dc75596e
wertwiktor <wertwiktor@gmail.com>
2014-08-19 11:05:31 +0200

    no to jedziemy z RMajem

--- games/src/pl/praktykiatrem/game/battleship/console/StartConsole.java
+++
@@ -1,9 +0,0 @@
-package pl.praktykiatrem.game.battleship.console;
-
-public class StartConsole {
-	public static void main(String[] args) {
-		BattleshipGame game = new BattleshipGame();
-		game.gameInProgress();
-
-	}
-}
--- games/src/pl/praktykiatrem/game/battleship/console/vsgraphic/test.java
+++
@@ -1,7 +0,0 @@
-package pl.praktykiatrem.game.battleship.console.vsgraphic;
-
-public class test {
-	public static void main(String[] args) {
-		new StartGraphicForConsole("Filip", "Wiktor", 1);
-	}
-}
